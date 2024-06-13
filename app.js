e5ce0fea852f8c85a50935d12e4ad2b582a839e2
JoaquinSola <ledex-full@hotmail.com>
2024-06-12 22:57:09 -0300

    Bienvenida

---
+++ app.js
@@ -0,0 +1,28 @@
+let intro = document.querySelector('.inicioPanel');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo1');
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.add('active');
+            }, (idx + 1) * 400);
+        });
+    });
+
+    setTimeout(() => {
+        logoSpan.forEach((span, idx) => {
+            setTimeout(() => {
+                span.classList.remove('active');
+                span.classList.add('fade');
+            }, (idx + 1) * 50);
+        });
+    }, 2000);
+
+    setTimeout(() => {
+        intro.style.top = "-100vh";
+        // Scroll to the top of the page
+        window.scrollTo(0, 0);
+    }, 2300);
+});
